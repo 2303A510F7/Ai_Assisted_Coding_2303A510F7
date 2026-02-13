7bceaf13430b6f4f6ebd4081b72afe0fa6375e80
2303A510F7 <2303A510F7@sru.edu.in>
2026-02-13 11:26:43 +0530

    Delete ass_2.2_2303A510F7.py

--- ass_2.2_2303A510F7.py
+++
@@ -1,7 +0,0 @@
-#ask 3: Palindrome Check
-def is_palindrome(s):
-    cleaned = ''.join(c.lower() for c in s if c.isalnum())
-    return cleaned == cleaned[::-1]
-# Test cases
-print(is_palindrome("A man a plan a canal Panama"))  # True
-print(is_palindrome("race a car"))  # False
